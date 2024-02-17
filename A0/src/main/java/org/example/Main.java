ad246ee91b3bed233dd0c7d6efc37345d948eb96
notarealone <alireza.hosseini.81@gmail.com>
2024-02-17 19:22:47 +0330

    A0:Step 3 example added

--- A0/src/main/java/org/example/Main.java
+++ A0/src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Date step_3 = new Date(18, 11, 1402);
+        System.out.println(step_3.nextDay().toString());
+
     }
 }
