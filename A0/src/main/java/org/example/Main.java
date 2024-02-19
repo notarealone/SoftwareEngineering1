1fcac52d96bd283d56db6bdc84d705c034428519
notarealone <alireza.hosseini.81@gmail.com>
2024-02-19 11:48:57 +0330

    A0: Examples completed and prints are DONE

--- A0/src/main/java/org/example/Main.java
+++ A0/src/main/java/org/example/Main.java
@@ -5,5 +5,13 @@ public class Main {
         Date step_3 = new Date(18, 11, 1402);
         System.out.println(step_3.nextDay().toString());
 
+        try {
+            CSVManager myManager = new CSVManager("src/main/data/input.csv");
+            myManager.readData();
+            System.out.println(myManager.countMembershipDays("Gholam", "Golgohar"));
+        }
+        catch (Exception e){
+            //Unreachable
+        }
     }
 }
