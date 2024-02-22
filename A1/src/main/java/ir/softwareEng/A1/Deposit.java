39493276ec296747cf8a4e6c87a06b3c1983f02b
notarealone <alireza.hosseini.81@gmail.com>
2024-02-22 17:30:56 +0330

    A1: getters added for Deposit class

--- A1/src/main/java/ir/softwareEng/A1/Deposit.java
+++ A1/src/main/java/ir/softwareEng/A1/Deposit.java
@@ -1,12 +1,15 @@
 package ir.softwareEng.A1;
 
 public class Deposit {
-    private String accountNo;
-    private int amount;
+    private final String accountNo;
+    private final int amount;
     public Deposit(String accountNo, int amount){
         this. accountNo = accountNo;
         this.amount = amount;
     }
+    public String getAccountNo(){ return this.accountNo; }
+    public int getAmount(){ return this.amount; }
+
     @Override
     public String toString(){
         return "DEPOSIT " + this.accountNo + " " + this.amount;
