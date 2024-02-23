e971c5c7536c22fd14866fb19e3018ca4e549ade
notarealone <alireza.hosseini.81@gmail.com>
2024-02-23 09:31:43 +0330

    A1: Controller added to recieve messages and respond accordingly

---
+++ A1/src/main/java/ir/softwareEng/A1/Controller.java
@@ -0,0 +1,60 @@
+package ir.softwareEng.A1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.Annotation;
+
+@Component
+public class Controller {
+    public static Accounting manager;
+    private final JmsTemplate jmsTemplate;
+    @Autowired
+    public Controller(JmsTemplate jmsTemplate){
+        this.jmsTemplate = jmsTemplate;
+    }
+
+    public void setManager(Accounting mng) {
+        manager = mng;
+    }
+    @JmsListener(destination = "INQ")
+    public void getMsg(String content){
+        String[] inputMsg = content.split("\\s+");
+        respond(inputMsg);
+    }
+
+    public void respond(String[] inputMsg){
+        switch (inputMsg[0]){
+            case "DEPOSIT" :
+                this.manager.deposit(inputMsg[1], Integer.parseInt(inputMsg[2]));
+                jmsTemplate.convertAndSend("OUTQ", "0 Deposit successful");
+                break;
+            case "WITHDRAW" :
+                switch(this.manager.withdraw(inputMsg[1], Integer.parseInt(inputMsg[2]))){
+                    case 0 :
+                        jmsTemplate.convertAndSend("OUTQ", "0 Withdraw successful");
+                        break;
+                    case 1 :
+                        jmsTemplate.convertAndSend("OUTQ", "1 Insufficient funds");
+                        break;
+                    case 2 :
+                        jmsTemplate.convertAndSend("OUTQ", "2 Unknown account number");
+                        break;
+                }
+                break;
+            case "BALANCE" :
+                int accountBalance = this.manager.getBalance(inputMsg[1]);
+                if(accountBalance < 0) {
+                    jmsTemplate.convertAndSend("OUTQ", "2 Unknown account number");
+                }
+                else {
+                    jmsTemplate.convertAndSend("OUTQ", "0 Balance: " + accountBalance);
+                }
+                break;
+        }
+    }
+    
+}
