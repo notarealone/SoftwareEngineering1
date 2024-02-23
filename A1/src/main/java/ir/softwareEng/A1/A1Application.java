f2d06e2775eb233960a272a3f573e9b22a720dbb
notarealone <alireza.hosseini.81@gmail.com>
2024-02-23 09:32:25 +0330

    A1: Application run commands added and manager created from the context

--- A1/src/main/java/ir/softwareEng/A1/A1Application.java
+++ A1/src/main/java/ir/softwareEng/A1/A1Application.java
@@ -1,13 +1,16 @@
 package ir.softwareEng.A1;
 
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class A1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(A1Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(A1Application.class, args);
+		Controller.manager = context.getBean(Accounting.class);
 	}
 
 }
