e3ef30bdcf3a911877139e03d36d1b2c385a0989
notarealone <alireza.hosseini.81@gmail.com>
2024-03-18 17:27:27 +0330

    A3: Fix: buyer's and seller's broker should be different so the changes to each broker's credit can be observed

--- A3/src/test/java/ir/ramtung/tinyme/domain/BrokerCreditTest.java
+++ A3/src/test/java/ir/ramtung/tinyme/domain/BrokerCreditTest.java
@@ -13,12 +13,15 @@ import org.springframework.test.annotation.DirtiesContext;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 @Import(MockedJMSTestConfig.class)
 @DirtiesContext
 public class BrokerCreditTest {
     private Security security;
-    private Broker broker;
+    private Broker sellerBroker;
+    private Broker buyerBroker;
     private Shareholder shareholder;
     private OrderBook orderBook;
     private List<Order> orders;
@@ -28,23 +31,25 @@ public class BrokerCreditTest {
     @BeforeEach
     void setupOrderBook() {
         security = Security.builder().build();
-        broker = Broker.builder().credit(100_000_000L).build();
+        sellerBroker = Broker.builder().credit(100_000_000L).build();
+        buyerBroker = Broker.builder().credit(100_000_000L).build();
         shareholder = Shareholder.builder().build();
         shareholder.incPosition(security, 100_000);
         orderBook = security.getOrderBook();
         orders = Arrays.asList(
-                new Order(1, security, Side.BUY, 304, 15700, broker, shareholder),
-                new Order(2, security, Side.BUY, 43, 15500, broker, shareholder),
-                new Order(3, security, Side.BUY, 445, 15450, broker, shareholder),
-                new Order(4, security, Side.BUY, 526, 15450, broker, shareholder),
-                new Order(5, security, Side.BUY, 1000, 15400, broker, shareholder),
-                new Order(6, security, Side.SELL, 350, 15800, broker, shareholder),
-                new Order(7, security, Side.SELL, 285, 15810, broker, shareholder),
-                new Order(8, security, Side.SELL, 800, 15810, broker, shareholder),
-                new Order(9, security, Side.SELL, 340, 15820, broker, shareholder),
-                new Order(10, security, Side.SELL, 65, 15820, broker, shareholder)
+                new Order(1, security, Side.BUY, 304, 15700, buyerBroker, shareholder),
+                new Order(2, security, Side.BUY, 43, 15500, buyerBroker, shareholder),
+                new Order(3, security, Side.BUY, 445, 15450, buyerBroker, shareholder),
+                new Order(4, security, Side.BUY, 526, 15450, buyerBroker, shareholder),
+                new Order(5, security, Side.BUY, 1000, 15400, buyerBroker, shareholder),
+
+                new Order(6, security, Side.SELL, 350, 15800, sellerBroker, shareholder),
+                new Order(7, security, Side.SELL, 285, 15810, sellerBroker, shareholder),
+                new Order(8, security, Side.SELL, 800, 15810, sellerBroker, shareholder),
+                new Order(9, security, Side.SELL, 340, 15820, sellerBroker, shareholder),
+                new Order(10, security, Side.SELL, 65, 15820, sellerBroker, shareholder)
         );
         orders.forEach(order -> orderBook.enqueue(order));
     }
-    
+
 }
