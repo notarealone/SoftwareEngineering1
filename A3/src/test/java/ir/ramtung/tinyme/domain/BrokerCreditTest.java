fcf4035dec0c5ddd874a19969f37873c51f845f6
notarealone <alireza.hosseini.81@gmail.com>
2024-03-19 21:10:15 +0330

    A3: removed junk codes

--- A3/src/test/java/ir/ramtung/tinyme/domain/BrokerCreditTest.java
+++ A3/src/test/java/ir/ramtung/tinyme/domain/BrokerCreditTest.java
@@ -20,7 +20,6 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@Import(MockedJMSTestConfig.class)
 @DirtiesContext
 public class BrokerCreditTest {
     private Security security;
